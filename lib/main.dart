813486eef46d05d1c3c27e1ebef7ba7b1cb76329
Hasan Shukurov <hasan.shukurof@gmail.com>
2024-07-22 21:55:06 +0400

    again fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 void main(List<String> args) {
   runApp(AdminPanel());
@@ -12,6 +15,23 @@ class AdminPanel extends StatefulWidget {
 }
 
 class _AdminPanelState extends State<AdminPanel> {
+  File? _image;
+  final picker = ImagePicker();
+
+  Future getImageGallery() async {
+    final pickedFile = await picker.pickImage(
+      source: ImageSource.gallery,
+      imageQuality: 80,
+    );
+    setState(() {
+      if (pickedFile != null) {
+        _image = File(pickedFile.path);
+      } else {
+        print("No Image Picked");
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -27,6 +47,35 @@ class _AdminPanelState extends State<AdminPanel> {
               color: Colors.white,
             ),
           ),
+        ),
+        body: Column(
+          children: [
+            Center(
+              child: InkWell(
+                onTap: () {
+                  getImageGallery();
+                },
+                child: Container(
+                  height: 200,
+                  width: 200,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black),
+                  ),
+                  child: _image != null
+                      ? Image.file(
+                          _image!.absolute,
+                          fit: BoxFit.cover,
+                        )
+                      : Center(
+                          child: Icon(
+                            Icons.add_photo_alternate_outlined,
+                            size: 60,
+                          ),
+                        ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
